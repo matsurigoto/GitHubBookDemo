c2bc1d97c476446e17a982157bb5e13a6b7c4c2e
Duran Hsieh <dog0416@gmail.com>
2022-03-12 10:54:08 +0800

    Update Program.cs

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -11,6 +11,7 @@ namespace WebApplication
 {
     public class Program
     {
+        //程式起始點
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
